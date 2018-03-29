937307d1a3398f07b31123753ac518968be91ab7
Sam Lin <maxisam@gmail.com>
2018-03-28 23:41:35 -0500

    fix: handle impersonate corrently

--- QuickstartIdentityServer/Service/ImpersonateAuthorizeInteractionResponseGenerator.cs
+++ QuickstartIdentityServer/Service/ImpersonateAuthorizeInteractionResponseGenerator.cs
@@ -1,39 +1,64 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using IdentityServer4;
 using IdentityServer4.Models;
 using IdentityServer4.ResponseHandling;
 using IdentityServer4.Services;
 using IdentityServer4.Validation;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 
 namespace QuickstartIdentityServer.Service
 {
     public class ImpersonateAuthorizeInteractionResponseGenerator : AuthorizeInteractionResponseGenerator
     {
-        public ImpersonateAuthorizeInteractionResponseGenerator(ISystemClock clock, ILogger<AuthorizeInteractionResponseGenerator> logger, IConsentService consent, IProfileService profile)
+        private readonly IHttpContextAccessor _http;
+
+        public ImpersonateAuthorizeInteractionResponseGenerator(
+            ISystemClock clock,
+            ILogger<AuthorizeInteractionResponseGenerator> logger,
+            IConsentService consent,
+            IProfileService profile,
+            IHttpContextAccessor http)
             : base(clock, logger, consent, profile)
         {
+            _http = http;
         }
 
         public override async Task<InteractionResponse> ProcessInteractionAsync(ValidatedAuthorizeRequest request, ConsentResponse consent = null)
         {
-            var acr = request.GetAcrValues();
-            // check if client is from admin and get the impersonate data from acr and put into Claim and update cookies
-            // Question: how to update existing cookie with new claim
-            if (request?.Client?.ClientId == "mvc.implicit")
+            var result = await base.ProcessLoginAsync(request);
+
+            if (result.IsLogin || result.IsError) return result;
+
+            var acr = request.GetAcrValues().FirstOrDefault(x => x.StartsWith("impersonate:"));
+            if (acr != null)
             {
-                var acr = request.GetAcrValues();
-                // // TODO: Do some other behind the scenes check
+                if (request.Subject.HasClaim("name", "alice"))
+                {
+                    var target = acr.Split(':')[1];
+                    if (target == "bob")
+                    {
+                        var newUser = new IdentityServerUser("2")
+                        {
+                            AdditionalClaims = {
+                                new Claim("orignal_sub", request.Subject.FindFirstValue("sub")),
+                            }
+                        }.CreatePrincipal();
 
-                // var claims = new[] { new Claim(JwtClaimTypes.Name, "Fred Blogs"), new Claim(JwtClaimTypes.FamilyName, "Blogs"), new
-                // Claim(JwtClaimTypes.GivenName, "Fred"), new Claim(JwtClaimTypes.Email, "fred.blogs@blogsandson.com"), };
-
-                // var newPrincipal = IdentityServerPrincipal.Create("fred.blogs", "Fred Blogs", claims); request.Subject = newPrincipal;
-
-                // return new InteractionResponse();
+                        await _http.HttpContext.SignInAsync(newUser);
+                        request.Subject = newUser;
+                        return new InteractionResponse
+                        {
+                            RedirectUrl = "http://localhost:5002"
+                        };
+                    }
+                }
             }
 
-            return await base.ProcessInteractionAsync(request, consent);
+            return result;
         }
 
         protected override async Task<InteractionResponse> ProcessConsentAsync(ValidatedAuthorizeRequest request, ConsentResponse consent = null)
